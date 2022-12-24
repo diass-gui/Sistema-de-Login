ea5179660c264eeb54a8c4307dca0e5459bf02f6
diass-gui <enriqueguilherme2@gmail.com>
2022-12-24 12:28:01 -0300

    projeto terminado.

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,6 @@
 <?php 
 //incluir arquivo de conexão
-require_once "configBD.php";
+require_once "php/configBD.php";
 
 //definindo variáveis 
 $username = $password = $confirm_password = "";
@@ -42,5 +42,97 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     //validando senha
+    if(empty(trim($_POST["password"]))) {
+        $password_err = "Por favor insira uma senha.";
+    } else if(strlen(trim($_POST["password"])) < 6) {
+        $password_err = "A senha deve ter pelo menos 6 caracteres.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    //validar e confirmar senha
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Por favor, confirme a senha.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && empty($password != $confirm_password)){
+            $confirm_password_err = "A senha não confere.";
+        }
+    }
+
+    //verificar erros de entrada antes de ir pro bd
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
+        //preparar declaração de inserção
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+
+        if($stmt = $pdo->prepare($sql)){
+            
+            //vincular as variáveis à instrução
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_username, PDO::PARAM_STR);
+
+            //definir parâmetros
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); //password hash criado
+
+            // tente executar a declaração preparada 
+            if($stmt->execute()){
+                
+                //redirecionar para a pagina de login
+                header("location: login.php");
+            } else {
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde";
+            }
+
+            //fechando declaração
+            unset($stmt);
+        }
+
+    }
+    //fechando conexão
+    unset($pdo);
 
 }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Cadastro</h2>
+        <p>Por favor, preencha este formulário para criar uma conta.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Nome do usuário</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Senha</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirme a senha</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Criar Conta">
+                <input type="reset" class="btn btn-secondary ml-2" value="Apagar Dados">
+            </div>
+            <p>Já tem uma conta? <a href="login.php">Entre aqui</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
---
+++ php/reset_senha.php
@@ -0,0 +1,107 @@
+<?php
+// Inicializando a sessão
+session_start();
+ 
+// Verifica se o usuário está logado, caso contrário, redireciona para a página de login
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+ 
+// Incluir arquivo de configuração
+require_once "config.php";
+ 
+// Definindo variáveis 
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Processando dados do formulário quando é enviado
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validando nova senha
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Por favor insira a nova senha.";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "A senha deve ter pelo menos 6 caracteres.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Valida e confirma a senha
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Por favor, confirme a senha.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "A senha não confere.";
+        }
+    }
+        
+    // Verifica os erros de entrada antes de atualizar o banco de dados
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepara uma declaração de atualização
+        $sql = "UPDATE users SET password = :password WHERE id = :id";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Vincula as variáveis à instrução 
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":id", $param_id, PDO::PARAM_INT);
+            
+            // Definindo parâmetros
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+            
+            // Tente executar a declaração 
+            if($stmt->execute()){
+                // Senha atualizada com sucesso. Destrua a sessão e redirecione para a página de login
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+
+            // Fechando declaração
+            unset($stmt);
+        }
+    }
+    
+    // Fechando conexão
+    unset($pdo);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Redefinir senha</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Redefinir senha</h2>
+        <p>Por favor, preencha este formulário para redefinir sua senha.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+            <div class="form-group">
+                <label>Nova senha</label>
+                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirme a senha</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Redefinir">
+                <a class="btn btn-link ml-2" href="welcome.php">Cancelar</a>
+            </div>
+        </form>
+    </div>    
+</body>
+</html>
